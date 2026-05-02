e0c2b9a4a455ffed91bfb6ceed8b8fc3bb06b060
Prince Sharma <princesharma4867@gmail.com>
2026-05-03 01:19:06 +0530

    2-May

---
+++ app/Http/Controllers/AnalyticsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnalyticsController extends Controller
+{
+    //
+    public function getTeacherDashboard(Request $request){
+
+    }
+    
+    public function courseAnalytics(Request $request, $course_id){
+        
+    }
+
+    public function getTeacherRevenueReport(Request $request){
+
+    }
+}
--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class SubmissionController extends Controller
 {
     //
+    public function createAssignmentGrade(Request $request, $submission_id){
+
+    }
+
+    public function getAssignmentSubmission(Request $request, $assignment_id){
+        
+    }
 }
