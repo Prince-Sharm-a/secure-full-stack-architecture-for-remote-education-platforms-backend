a6cb00ba82c5248ba843387a0ff6dd92df484fe8
Prince Sharma <150456567+Prince-Sharm-a@users.noreply.github.com>
2026-05-10 00:37:59 +0530

    initial commit

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
     //
-    public function getModulesLessonTeacher(Request $request, $module_id){
+    public function getModulesLesson(Request $request, $module_id){
         try{
+            
+            $data = Lesson::where('module_id','=',$module_id)->select(['id','module_id','title'])->get();
 
+            if(!$data){
+                return response()->json(['success' => false,'message' => 'Not Found','data' => []],404);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => '',
+                'data' => $data
+            ]);
         } catch(\Exception $e) {
             return response()->json([
                 'success' => false,
@@ -35,7 +47,19 @@ class LessonController extends Controller
 
     public function updateLessonTeacher(Request $request, $id){
         try{
+            $user = $request->user();
+            $data = Lesson::leftJoin('modules','modules.id','=','lessons.module_id')->leftJoin('courses','courses.id','=','modules.course_id')->where('courses.teacher_id','=',$user->id)->where('lessons.id','=',$id)->select(['lessons.id','lessons.module_id','lessons.title','lessons.video_url'])->first();
 
+            if(!$data){
+                return response()->json(['success' => false,'message' => 'Not Found','data' => []],404);
+            }
+            $data->update($request->only('title','video_url'));
+
+            return response()->json([
+                'success' => true,
+                'message' => '',
+                'data' => $data
+            ]);
         } catch(\Exception $e) {
             return response()->json([
                 'success' => false,
