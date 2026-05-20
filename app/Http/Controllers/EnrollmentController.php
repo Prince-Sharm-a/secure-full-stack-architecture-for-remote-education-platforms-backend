7c2c9523f697ab2236279200b0341d93eff4b5cb
Prince Sharma <princesharma4867@gmail.com>
2026-05-20 10:51:37 +0530

    20-May

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Enrollment;
 use Illuminate\Http\Request;
 
 class EnrollmentController extends Controller
@@ -36,6 +37,24 @@ class EnrollmentController extends Controller
     public function getEnrollmentsByCourse(Request $request, $course_id){
         try{
 
+        } catch(\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'error' => $e->getMessage(),
+                'data' => []
+            ],500);
+        }
+    }
+    public function getEnrolledStudent(Request $request, $course_id){
+        try{
+            $data = Enrollment::where('course_id','=',$course_id)->select(['user_id','progress','enrolled_at'])->with(['students:id,name,email,phone'])->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => '',
+                'data' => $data
+            ]);
         } catch(\Exception $e) {
             return response()->json([
                 'success' => false,
--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -10,4 +10,10 @@ class Enrollment extends Model
     protected $fillable = [
         'user_id','course_id','progress','enrolled_at'
     ];
+    public function courses(){
+        return $this->belongsTo(Course::class,'course_id','id');
+    }
+    public function students(){
+        return $this->belongsTo(Enrollment::class,'user_id','id');
+    }
 }
